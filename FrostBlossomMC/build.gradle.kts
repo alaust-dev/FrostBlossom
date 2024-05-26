ff329453315cded8fdd19cc5908f7d79de0e5eea
Alexander Aust <mail@alexander-aust.de>
2024-05-26 19:27:53 +0200

    Adds maven-publish plugin

--- FrostBlossomMC/build.gradle.kts
+++ FrostBlossomMC/build.gradle.kts
@@ -3,10 +3,11 @@ plugins {
     kotlin("jvm")
     id("xyz.jpenilla.run-paper") version "2.3.0"
     id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("maven-publish")
 }
 
-group = "de.alaust.frostblossommc"
-version = "1.0-SNAPSHOT"
+group = "de.alaust"
+version = "1.0.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -16,12 +17,33 @@ repositories {
     }
 }
 
+publishing {
+    publications {
+        publications {
+            create<MavenPublication>("gpr") {
+                from(components["java"])
+
+                artifactId = "frostblossommc"
+            }
+        }
+
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/alaust-dev/FrostBlossomMC")
+                credentials {
+                    username = System.getenv("GH_USERNAME")
+                    password = System.getenv("GH_TOKEN")
+                }
+            }
+        }
+    }
+}
+
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.reflections:reflections:0.10.2")
-
     compileOnly("io.papermc.paper:paper-api:1.20.6-R0.1-SNAPSHOT")
-
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
@@ -31,6 +53,7 @@ tasks {
         useJUnitPlatform()
     }
 }
+
 kotlin {
     jvmToolchain(21)
 }
